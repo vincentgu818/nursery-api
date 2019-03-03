b28ab29be32df872ac32d886f77de30ca527b027
vincentgu818 <vincentgu818@gmail.com>
2019-03-03 14:37:48 -0500

    database config for heroku

--- app/models/feeding.rb
+++ app/models/feeding.rb
@@ -1,9 +1,10 @@
 class Feeding
-  DB = PG.connect({
-    :host=> "localhost",
-    :port => 5432,
-    :dbname => 'nursery-api_development'
-  })
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'nursery-api_development')
+    end
 
   def self.all
     results = DB.exec(
